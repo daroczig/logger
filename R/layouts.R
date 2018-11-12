04ce806174e65ce1ad3e594921ea3f88a96037b4
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-11-13 00:47:40 +0100

    create a layout generator

--- R/layouts.R
+++ R/layouts.R
@@ -1,19 +1,39 @@
-#' Formats a log message
+#' Generating logging function
 #' @param level log level, eg \code{INFO}
 #' @param msg character vector
+#' @param msg_format \code{glue}-flavored layout of the message
+#' @param time_format see \code{strptime} for details
+#' @return function taking \code{level} and \code{msg} arguments
+#' @importFrom glue glue
+#' @export
+layout_generator <- function(msg_format = '{level} [{time}] {msg}',
+                             time_format = '%Y-%d-%m %H:%M:%S') {
+
+    force(msg_format)
+    force(time_format)
+
+    function(level, msg) {
+
+        if (!inherits(level, 'loglevel')) {
+            stop('Invalid log level, see ?log_levels')
+        }
+
+        time  <- as.character(Sys.time(), time_format)
+        level <- attr(level, 'level')
+        msg   <- glue(msg)
+
+        glue(msg_format)
+
+    }
+
+}
+
+
+#' Formats a log message
+#' @inheritParams layout_generator
 #' @return character vector
 #' @importFrom glue glue
 #' @export
-layout_glue <- function(level, msg) {
-
-    if (!inherits(level, 'loglevel')) {
-        stop('Invalid log level, see ?log_levels')
-    }
-
-    time  <- as.character(Sys.time(), '%Y-%d-%m %H:%M:%S')
-    level <- attr(level, 'level')
-    msg   <- glue(msg)
-
-    glue('{level} [{time}] {msg}')
-
-}
+layout_glue <- layout_generator(
+    msg_format = '{level} [{time}] {msg}',
+    time_format = '%Y-%d-%m %H:%M:%S')
