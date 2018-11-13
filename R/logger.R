c0fd7d48b8e36b9e2b4d48f7cc09fddd0bd1bd5b
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-11-13 02:02:09 +0100

    log info

--- R/logger.R
+++ R/logger.R
@@ -16,11 +16,29 @@ logger <- function(threshold, layout, appender) {
             stop('Invalid log level provided as threshold, see ?log_levels')
         }
 
-        if (level >= threshold) {
+        if (level > threshold) {
             return(invisible(NULL))
         }
 
         appender(layout(level, msg))
 
     }
+}
+
+
+#' Find the logger used in the current namespace
+#' @return function
+#' @keywords internal
+get_logger <- function() {
+    ## TODO actually find instead of static
+    namespaces$global
+}
+
+
+#' Log a message with given log level
+#' @param level log level from \code{log_levels}
+#' @param msg character vector
+#' @export
+log <- function(level, msg) {
+    get_logger()(level, msg)
 }
