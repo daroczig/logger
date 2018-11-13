6ef1a7a1cfea09b2d158ce7c142c5762529ac1fd
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-11-13 17:17:42 +0100

    define 2 new appenders to write to file

--- R/appenders.R
+++ R/appenders.R
@@ -5,6 +5,28 @@ appender_console <- function(lines) {
     cat(lines, sep = '\n')
 }
 
-## TODO file
+
+#' Append log messages to a file
+#' @param file path
+#' @export
+#' @return function taking \code{lines} argument
+appender_file <- function(file) {
+    function(lines) {
+        cat(lines, sep = '\n', file = file, append = TRUE)
+    }
+}
+
+
+#' Append log messages to a file and stdout as well
+#' @param file path
+#' @export
+#' @return function taking \code{lines} argument
+appender_tee <- function(file) {
+    function(lines) {
+        appender_console(lines)
+        appender_file(file)(lines)
+    }
+}
+
 
 ## TODO graylog, cloudwatch, datadog, kinesis etc
