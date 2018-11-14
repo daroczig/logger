585a07cfac96c5a3ee9f10c3afb7d99e93a0e60c
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-11-14 02:20:07 +0100

    add dummy package to be use to demonstrate pkg namespaces

---
+++ inst/tests/logger-tester-package/R/tester.R
@@ -0,0 +1,8 @@
+#' Testing logging from package
+#' @param level foo
+#' @param msg bar
+#' @export
+#' @importFrom logger log
+logger.tester.function <- function(level, msg) {
+    log(level, msg)
+}
