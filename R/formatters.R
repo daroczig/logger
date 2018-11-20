89b44852bce2a08b6ddcbb901a65660f162bb272
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-11-20 15:24:20 +0100

    add a more complex formatter applying both sprintf and glue

--- R/formatters.R
+++ R/formatters.R
@@ -1,4 +1,4 @@
-#' Concatenate strings
+#' Concatenate strings via \code{paste}
 #' @param ... R objects that can be converted to string
 #' @return string
 #' @export
@@ -21,5 +21,49 @@ formatter_sprintf <- function(fmt, ...) {
 #' @return character vector
 #' @export
 formatter_glue <- function(...) {
-    glue(...)
+    as.character(glue(...))
+}
+
+
+#' Apply \code{glue} and \code{sprintf}
+#' @param ... passed to \code{glue} for the text interpolation
+#' @return character vector
+#' @export
+formatter_glue_or_sprinf <- function(msg, ...) {
+
+    params <- list(...)
+
+    ## params without a name are potential sprintf params
+    sprintfparams <- which(names(params) == '')
+    if (length(params) > 0 & length(sprintfparams) == 0) {
+        sprintfparams <- seq_along(params)
+    }
+
+    ## but some unnamed params might belong to glue actually, so
+    ## let's look for the max number of first unnamed params sprintf expects
+    sprintftags <- regmatches(msg, gregexpr('%[0-9.+0]*[aAdifeEgGosxX]', msg))[[1]]
+    sprintfparams <- sprintfparams[seq_len(min(length(sprintftags), length(sprintfparams)))]
+
+    ## get the actual params instead of indexes
+    glueparams    <- params[setdiff(seq_along(params), sprintfparams)]
+    sprintfparams <- params[sprintfparams]
+
+    ## first try to apply sprintf
+    if (length(sprintfparams) > 0) {
+        sprintfparams[vapply(sprintfparams, is.null, logical(1))] <- 'NULL'
+        msg <- tryCatch(
+            do.call(sprintf, c(msg, sprintfparams)),
+            error = function(e) msg)
+    }
+
+    ## then try to glue
+    msg <- tryCatch(
+        as.character(sapply(msg, function(msg) {
+            do.call(glue, c(msg, glueparams))
+        }, USE.NAMES = FALSE)),
+        error = function(e) msg)
+
+    ## return
+    msg
+
 }
