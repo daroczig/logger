3eb5adac89bb8c80d5d92bee8c4fee59d7dd19fc
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-12-17 00:06:35 +0100

    warn in onAttach instead of onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -3,20 +3,24 @@ namespaces <- new.env()
 
 .onLoad <- function(libname, pkgname) {
 
-    default_formatter <- formatter_glue
-    ## warn user about using sprintf instead of glue due to missing dependency
-    if (!requireNamespace('glue', quietly = TRUE)) {
-        packageStartupMessage('logger: As the "glue" R package is not installed, using "sprintf" as the default log message formatter.')
-        default_formatter <- formatter_sprintf
-    }
-
     ## default namespace's logger settings
     namespaces$global <- list(
         ## there can be multiple loggers for a namespace
         default = list(
             threshold = INFO,
             layout    = layout_simple,
-            formatter = default_formatter,
+            formatter = formatter_sprintf,
             appender  = appender_console))
+
+}
+
+.onAttach <- function(libname, pkgname) {
+
+    ## warn user about using sprintf instead of glue due to missing dependency
+    if (!requireNamespace('glue', quietly = TRUE)) {
+        packageStartupMessage('logger: As the "glue" R package is not installed, using "sprintf" as the default log message formatter instead of "glue".')
+    } else {
+        log_formatter(formatter_glue, namespace = 'global', index = 1)
+    }
 
 }
