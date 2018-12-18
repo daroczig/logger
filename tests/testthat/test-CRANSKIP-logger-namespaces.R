f64b29a5786f8192157413b480cb2d5080536087
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-12-18 01:19:18 +0100

    test calls from package for namespace

--- tests/testthat/test-CRANSKIP-logger-namespaces.R
+++ tests/testthat/test-CRANSKIP-logger-namespaces.R
@@ -8,52 +8,71 @@ test_that('namespace in a remote R session to avoid calling from testthat', {
     t <- tempfile()
     cat('
       library(logger)
-      log_layout(layout_glue_generator("{ns} / {topenv} / {fn} / {call}"))
+      log_layout(layout_glue_generator("{ns} / {ans} / {topenv} / {fn} / {call}"))
       log_info("foobar")', file = t)
     expect_equal(
         system(paste('Rscript', t), intern = TRUE),
-        'global / R_GlobalEnv / NA / NA')
+        'global / global / R_GlobalEnv / NA / NA')
     unlink(t)
 
     t <- tempfile()
     cat('
       library(logger)
-      log_layout(layout_glue_generator("{ns} / {topenv} / {fn} / {call}"))
+      log_layout(layout_glue_generator("{ns} / {ans} / {topenv} / {fn} / {call}"))
       f <- function() log_info("foobar")
       f()', file = t)
     expect_equal(
         system(paste('Rscript', t), intern = TRUE),
-        'global / R_GlobalEnv / f / f()')
+        'global / global / R_GlobalEnv / f / f()')
     unlink(t)
 
     t <- tempfile()
     cat('
       library(logger)
-      log_layout(layout_glue_generator("{ns} / {topenv} / {fn} / {call}"))
+      log_layout(layout_glue_generator("{ns} / {ans} / {topenv} / {fn} / {call}"))
       f <- function() log_info("foobar")
       g <- function() f()
       g()', file = t)
     expect_equal(
         system(paste('Rscript', t), intern = TRUE),
-        'global / R_GlobalEnv / f / f()')
+        'global / global / R_GlobalEnv / f / f()')
     unlink(t)
 
     t <- tempfile()
     cat('
       library(logger)
-      log_layout(layout_glue_generator("{ns} / {topenv} / {fn} / {call}"))
+      log_layout(layout_glue_generator("{ns} / {ans} / {topenv} / {fn} / {call}"))
       f <- function() log_info("foobar")
       g <- f
       g()', file = t)
     expect_equal(
         system(paste('Rscript', t), intern = TRUE),
-        'global / R_GlobalEnv / g / g()')
+        'global / global / R_GlobalEnv / g / g()')
     unlink(t)
 
-    ## TODO add tester pkg examples
-    ## devtools::load_all(system.file('demo-packages/logger-tester-package', package = 'logger'))
-    ## log_layout(layout_simple)
-    ## expect_output(logger_tester_function(INFO, 'x = '), 'INFO')
-    ## expect_output(logger_info_tester_function('everything = '), 'INFO')
+    t <- tempfile()
+    cat('
+      library(logger)
+      log_layout(layout_glue_generator("{ns} / {ans} / {topenv} / {fn} / {call}"))
+      devtools::load_all(system.file("demo-packages/logger-tester-package", package = "logger"))
+      logger_info_tester_function("foobar")',
+      file = t)
+    expect_equal(
+        system(paste('Rscript', t), intern = TRUE),
+        'logger.tester / global / logger.tester / logger_info_tester_function / logger_info_tester_function("foobar")')
+    unlink(t)
+
+    t <- tempfile()
+    cat('
+      library(logger)
+      log_layout(layout_glue_generator("{ns} / {ans} / {topenv} / {fn} / {call}"))
+      devtools::load_all(system.file("demo-packages/logger-tester-package", package = "logger"))
+      log_threshold(INFO, namespace = "logger.tester")
+      logger_info_tester_function("foobar")',
+      file = t)
+    expect_equal(
+        system(paste('Rscript', t), intern = TRUE),
+        'logger.tester / logger.tester / logger.tester / logger_info_tester_function / logger_info_tester_function("foobar")')
+    unlink(t)
 
 })
