f9a7ef54cc544b90759de3d6f7af9295fea568b2
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2019-01-05 21:25:37 +0100

    add binary operator for tryCatch with logging

---
+++ R/try.R
@@ -0,0 +1,35 @@
+#' Try to evaluate an expressions and evaluate another expression on exception
+#' @param try R expression
+#' @param except fallback R expression to be evaluated if \code{try} fails
+#' @export
+#' @note Suppress log messages in the \code{except} namespace if you don't want to throw a \code{WARN} log message on the exception branch.
+#' @examples
+#' xxx %except% 42
+#' xxx <- 13
+#' xxx %except% 42
+#' rm(xxx)
+#' Mean(1:10) %except% sum(1:10) / length(1:10)
+#' Mean(1:10) %except% (sum(1:10) / length(1:10))
+#' Mean(1:10) %except% MEAN(1:10) %except% mean(1:10)
+`%except%` <- function(try, except) {
+
+    call <- sys.call(-1)
+    env <- parent.frame()
+    try <- substitute(try)
+    fallback <- substitute(except)
+
+    tryCatch(
+        eval(try, envir = env),
+        error = function(e) {
+            log_level(
+                WARN,
+                paste(
+                    'Running', shQuote(deparse(fallback)), 'as',
+                    shQuote(deparse(try)), 'failed:',
+                    shQuote(e$message)),
+                namespace = 'except',
+                .topcall = call, .topenv = env)
+            eval(fallback, envir = env)
+        })
+
+}
