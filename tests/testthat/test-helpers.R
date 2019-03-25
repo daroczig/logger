f8a87cb3c39026e17a805a556edb270a205f0f2e
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2019-03-26 00:42:43 +0100

    add tests for new helpers

---
+++ tests/testthat/test-helpers.R
@@ -0,0 +1,16 @@
+library(logger)
+library(testthat)
+
+context('helpers')
+
+test_that('separator', {
+    expect_output(log_separator(), '===')
+    expect_output(log_separator(separator = '-'), '---')
+    expect_output(log_separator(), 'INFO')
+    expect_output(log_separator(WARN), 'WARN')
+})
+
+test_that('tictoc', {
+    expect_output(log_tictoc(), 'timer tic 0 secs')
+    expect_output(log_tictoc(), 'timer toc')
+})
