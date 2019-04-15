111e90506decb9ab68a119c9671fefb03d737ae3
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2019-04-15 13:48:01 +0200

    add helper functions to automatically log messages/warnings/errors

---
+++ R/hooks.R
@@ -0,0 +1,52 @@
+#' Injects a logger call to standard messages
+#'
+#' This function uses \code{trace} to add a \code{log_info} function call when \code{message} is called to log the informative messages with the \code{logger} layout and appender.
+#' @return
+#' @export
+#' @examples \dontrun{
+#' log_messages()
+#' message('hi there')
+#' }
+log_messages <- function() {
+    invisible(suppressMessages(trace(
+        what = 'message',
+        trace = substitute(logger::log_info(logger::skip_formatter(paste(list(...), collapse = '')))),
+        print = FALSE,
+        where = baseenv())))
+}
+
+
+#' Injects a logger call to standard warnings
+#'
+#' This function uses \code{trace} to add a \code{log_warn} function call when \code{warning} is called to log the warning messages with the \code{logger} layout and appender.
+#' @return
+#' @export
+#' @examples \dontrun{
+#' log_warnings()
+#' for (i in 1:5) { Sys.sleep(runif(1)); warning(i) }
+#' }
+log_warnings <- function() {
+    invisible(suppressMessages(trace(
+        what = 'warning',
+        trace = substitute(logger::log_warn(logger::skip_formatter(paste(list(...), collapse = '')))),
+        print = FALSE,
+        where = baseenv())))
+}
+
+
+#' Injects a logger call to standard errors
+#'
+#' This function uses \code{trace} to add a \code{log_error} function call when \code{stop} is called to log the error messages with the \code{logger} layout and appender.
+#' @return
+#' @export
+#' @examples \dontrun{
+#' log_errors()
+#' stop('foobar')
+#' }
+log_errors <- function() {
+    invisible(suppressMessages(trace(
+        what = 'stop',
+        trace = substitute(logger::log_error(logger::skip_formatter(paste(list(...), collapse = '')))),
+        print = FALSE,
+        where = baseenv())))
+}
