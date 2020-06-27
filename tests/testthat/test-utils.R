484e46391da08ff56c25b9e2a720e6d01f047126
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2020-06-27 15:26:20 +0200

    test %except%

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,9 +1,21 @@
 library(logger)
 library(testthat)
 
+## save current settings so that we can reset later
+appender <- log_appender()
+log_appender(appender_stdout)
+
 context('utils')
 
 test_that('fail_on_missing_package', {
     expect_error(fail_on_missing_package('logger'), NA)
     expect_error(fail_on_missing_package('an.R.package-that-doesNotExists'))
 })
+
+test_that('except helper', {
+    expect_equal(Mean(1:10) %except% sum(1:10) / length(1:10), 5.5)
+    expect_output(Mean(1:10) %except% sum(1:10) / length(1:10), 'WARN')
+})
+
+## reset settings
+log_appender(appender)
