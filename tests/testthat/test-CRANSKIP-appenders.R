703d6f3ca838460f40b1cfa5b061b3ee0eb3f94d
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2021-10-18 22:44:20 +0200

    wait more

--- tests/testthat/test-CRANSKIP-appenders.R
+++ tests/testthat/test-CRANSKIP-appenders.R
@@ -14,7 +14,7 @@ test_that('async logging', {
     log_appender(my_appender)
     log_layout(layout_blank)
     for (i in 1:5) log_info(i)
-    Sys.sleep(0.1)
+    Sys.sleep(0.25)
     expect_equal(readLines(t)[1], '1')
     expect_equal(length(readLines(t)), 5)
     unlink(t)
