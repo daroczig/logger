b31b4dc5a1598d53948a70f5ef977ff42784fcd7
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-10-09 15:08:32 +0100

    increase sleep time (for codecov)

--- tests/testthat/test-CRANSKIP-appenders.R
+++ tests/testthat/test-CRANSKIP-appenders.R
@@ -8,7 +8,7 @@ test_that("async logging", {
   )
 
   for (i in 1:5) log_info(i)
-  Sys.sleep(0.25)
+  Sys.sleep(0.5)
 
   expect_equal(readLines(t)[1], "1")
   expect_equal(length(readLines(t)), 5)
