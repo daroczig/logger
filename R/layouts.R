9b0a9e7292873d35521156157e68d5781f631ae1
atusy <30277794+atusy@users.noreply.github.com>
2025-01-25 23:21:03 +0900

    feat: support formatting timestamp with layout_simple/logging

--- R/layouts.R
+++ R/layouts.R
@@ -150,7 +150,18 @@ layout_simple <- function(level,
                           .logcall = sys.call(),
                           .topcall = sys.call(-1),
                           .topenv = parent.frame()) {
-  paste0(attr(level, "level"), " [", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "] ", msg)
+  timestamp <- logger_meta_env(
+    log_level = level,
+    namespace = namespace,
+    .logcall = .logcall,
+    .topcall = .topcall,
+    .topenv = .topenv
+  )$time
+  paste0(attr(level, "level"), " [", if (is.character(timestamp)) {
+    timestamp
+  } else {
+    format(timestamp, "%Y-%m-%d %H:%M:%S")
+  }, "] ", msg)
 }
 attr(layout_simple, "generator") <- quote(layout_simple())
 
@@ -184,8 +195,14 @@ layout_logging <- function(level,
     .topcall = .topcall,
     .topenv = .topenv
   )
+  timestamp <- meta$time
   paste0(
-    format(Sys.time(), "%Y-%m-%d %H:%M:%S"), " ",
+    if (is.character(timestamp)) {
+      timestamp
+    } else {
+      format(timestamp, "%Y-%m-%d %H:%M:%S")
+    },
+    " ",
     attr(level, "level"), ":",
     ifelse(meta$ns == "global", "", meta$ns), ":",
     msg
