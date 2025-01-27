25050e3c5fa561bdf0dca58ba0370e99335e6d8a
Thomas Lin Pedersen <thomasp85@gmail.com>
2025-01-27 15:24:07 +0100

    try with normalizePath

--- R/utils.R
+++ R/utils.R
@@ -61,7 +61,7 @@ log_call_location <- function(.logcall) {
       loc$line <- ref[1L]
       file <- attr(ref, "srcfile")
       if (!is.null(file)) {
-        loc$path <- gsub("\\\\+", "/", file$filename)
+        loc$path <- normalizePath(file$filename, winslash = "/")
       }
       break
     }
